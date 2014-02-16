cd5f98c78a0e8e1362d642acc8c19a953a990fc8
Aleksandra Nabożny <aleksandra.nabozny@gmail.com>
2014-02-16 19:54:39 +0100

    index view and controller created

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,5 +1,24 @@
 class DataController < ApplicationController
-  def create
-    binding.pry
+  def send_data
+    item = Item.new item_params
+    item.date = Time.now
+    item.save
+
+    render :json => item
   end
+
+  def show_json
+    items = Item.all
+
+    render :json => items
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  private
+    def item_params
+      params.permit(:name, :title)
+    end
 end
